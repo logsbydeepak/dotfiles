ef99f434aae54d6a2d7b8edf9d69af055f988af1
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-06-03 21:19:05 +0530

    refactor lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -19,50 +19,23 @@ return {
     lazy = false,
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-
-      lspconfig.tsserver.setup({
-        capabilites = capabilities,
-      })
-
-      lspconfig.html.setup({
-        capabilites = capabilities,
-      })
-
-      lspconfig.lua_ls.setup({
-        capabilites = capabilities,
-      })
-
-      lspconfig.gopls.setup({
-        capabilites = capabilities,
-      })
-
-      lspconfig.tailwindcss.setup({
-        capabilites = capabilities,
-        settings = {
-          tailwindCSS = {
-            experimental = {
-              classRegex = {
-                {
-                  "cva\\(([^)]*)\\)",
-                  "[\"'`]([^\"'`]*).*?[\"'`]",
-                },
-                {
-                  "cn\\(([^)]*)\\)",
-                  "[\"'`]([^\"'`]*).*?[\"'`]",
-                },
-                { "tw`(.*?)`" },
-              },
-            },
-          },
-        },
-      })
+      require("mason-lspconfig").setup {
+        ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "tsserver", "html", "tailwindcss" },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup {
+              capabilites = capabilities,
+            }
+          end,
+        }
+      }
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
     end,
   },
   {
